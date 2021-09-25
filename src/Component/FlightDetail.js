98d36e835e8eb05d897fb444c63bbd6bca53fcfc
Farihaakter <yellowfariha@gmail.com>
2021-09-25 16:27:20 +0600

    added card and details of the flights

--- src/Component/FlightDetail.js
+++ src/Component/FlightDetail.js
@@ -15,21 +15,22 @@ const FlightDetail = () => {
                 {
                     flights ? flights.map(flight =>
                         <Col>
-                            <Card className='mb-5' border="success" style={{ width: '18rem' }}>
-                                <Card.Header>{flight.rocket.rocket_name}</Card.Header>
+                            <Card className='mb-5 p-3 shadow bg-body rounded' style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={flight.links.mission_patch_small} />
+
                                 <Card.Body>
-                                    <Card.Title>{flight.launch_year}</Card.Title>
+                                    <Card.Title className='fs-4 fw-bold'>{flight.rocket.rocket_name} - ({flight.launch_year})</Card.Title>
                                     <Card.Text className='text-muted'>
                                         <span className='fw-bold'> Launch Status : </span>
-                                        {flight?.launch_success !== true ? 'Failure' : 'Success' }
-                                        
+                                        {flight?.launch_success !== true ? 'Failure' : 'Success'}
+
                                     </Card.Text>
                                     <Card.Subtitle className='text-muted'>
-                                        <span className='fw-bold'> Upcoming : </span>
-                                        {flight?.upcoming !== true ? 'False' : 'True' }
-                                        
+                                        <span className='fw-bold'> Upcoming Status : </span>
+                                        {flight?.upcoming !== true ? 'False' : 'True'}
+
                                     </Card.Subtitle>
-                                    
+
                                 </Card.Body>
                             </Card>
                         </Col>
